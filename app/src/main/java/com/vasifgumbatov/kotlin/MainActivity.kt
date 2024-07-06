ac47a9769762d9fb02ba804c995ac4cbb534f6fb
vasifgumbatov <gumbatovvasif4@gmail.com>
2024-07-06 15:52:10 +0400

    homework 15 commit

--- app/src/main/java/com/vasifgumbatov/kotlin/MainActivity.kt
+++ app/src/main/java/com/vasifgumbatov/kotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vasifgumbatov.kotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.homework15_task1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
